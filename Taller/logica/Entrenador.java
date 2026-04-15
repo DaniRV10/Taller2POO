79cbfc09340472bc12a03f7a306a7ba4459d9f1d
DaniRV10 <daniel.robles@alumnos.ucn.cl>
2026-04-14 20:55:09 -0400

    Creacion de nuevas clases con sus constructores

---
+++ Taller/logica/Entrenador.java
@@ -0,0 +1,36 @@
+package logica;
+
+import java.util.ArrayList;
+
+public class Entrenador {
+	
+	private String nombre;
+	private ArrayList<String> lideresDerrotados;
+	private ArrayList<Pokemon> misPokemon;
+	
+	public Entrenador(String nombre) {
+		
+		this.nombre = nombre;
+		this.lideresDerrotados = new ArrayList<>();
+		this.misPokemon = new ArrayList<>();
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+	
+	public void añadirPokemon(Pokemon p) {
+		for(Pokemon miPokemon : misPokemon) {
+			if(miPokemon.getNombre().equalsIgnoreCase(p.getNombre())){
+				System.out.println("Ya tienes este Pokemon");
+				return;
+			}
+			
+		}
+		misPokemon.add(p);
+	}
+	
+	
+	
+
+}
---
+++ Taller/logica/Pokemon.java
@@ -0,0 +1,55 @@
+package logica;
+
+public class Pokemon {
+	private String nombre;
+	private String habitat;
+	private double porcentajeAparicion;
+	private int vida,ataque,defensa,SpAtaque,SpDefensa,velocidad;
+	private String tipo;
+	private String estado;
+	public Pokemon(String nombre, String habitat, double porcentajeAparicion, int vida, int ataque, int defensa,
+			int spAtaque, int spDefensa, int velocidad, String tipo) {
+		super();
+		this.nombre = nombre;
+		this.habitat = habitat;
+		this.porcentajeAparicion = porcentajeAparicion;
+		this.vida = vida;
+		this.ataque = ataque;
+		this.defensa = defensa;
+		SpAtaque = spAtaque;
+		SpDefensa = spDefensa;
+		this.velocidad = velocidad;
+		this.tipo = tipo;
+		this.estado = "Vivo";
+	}
+	
+	public int sumaStats() {
+		return vida + ataque + defensa + SpAtaque + SpDefensa + velocidad;
+	}
+
+	public String getEstado() {
+		return estado;
+	}
+
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public String getHabitat() {
+		return habitat;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+	
+	
+	
+	
+	
+
+}
